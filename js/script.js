76e72fdc200959477641e2b22b06136fe420c383
Zhumagul01 <Jumagulrysbekkyzy@gmail.com>
2022-05-19 16:50:50 +1100

    first

---
+++ js/script.js
@@ -0,0 +1,115 @@
+let pic = document.querySelector('.pictures')
+let cat = document.querySelector('.cat')
+let dog = document.querySelector('.dog')
+let horse = document.querySelector('.horse')
+let nature = document.querySelector('.nature')
+let fruits = document.querySelector('.fruits')
+let vegetables = document.querySelector('.vegetables')
+let pop = document.querySelector('.pop')
+let photo = document.querySelectorAll('.photo')
+// let h1 = document.querySelector('h1')
+// let btn = document.querySelector('.btn')
+// let body  = document.querySelector('body')
+
+let question = prompt('ВВЕДИТЕ НАЗВАНИЕ КАРТИНКИ?')
+let bd = document.body
+
+
+    if(question=='cat'){
+        cat.style.display = 'block'
+    }else if(question=='dog'){
+        dog.style.display='block'
+    }
+
+
+// let all = prompt('What picture do you want to see?')
+        // cat.style.display = 'none'
+        // dog.style.display = 'none'
+        // horse.style.display = 'none'
+        // nature.style.display = 'none'
+        // fruits.style.display = 'none'
+        // vegetables.style.display = 'none'
+        // pop.style.display = 'none'
+
+        
+
+
+// btn.addEventListener('click', () =>{
+//     all  = prompt('What picture do you want to see?')
+
+//     if(all == 'cat'){
+//         cat.style.display = 'block'
+//         // dog.style.display = 'none'
+//         // horse.style.display = 'none'
+//         // nature.style.display = 'none'
+//         // fruits.style.display = 'none'
+//         // vegetables.style.display = 'none'
+//         // pop.style.display = 'none'
+
+
+//     }else if(all == 'dog'){
+//         dog.style.display = 'block'
+//         // cat.style.display = 'none'
+//         // horse.style.display = 'none'
+//         // nature.style.display = 'none'
+//         // fruits.style.display = 'none'
+//         // vegetables.style.display = 'none'
+//         // pop.style.display = 'none'
+
+//     }else if(all == 'horse'){
+//         horse.style.display = 'block'
+//         cat.style.display = 'none'
+//         dog.style.display = 'none'
+//         nature.style.display = 'none'
+//         fruits.style.display = 'none'
+//         vegetables.style.display = 'none'
+//         pop.style.display = 'none'
+//     }else if(all == 'nature'){
+//         nature.style.display = 'block'
+//         cat.style.display = 'none'
+//         dog.style.display = 'none'
+//         horse.style.display = 'none'
+//         fruits.style.display = 'none'
+//         vegetables.style.display = 'none'
+//         pop.style.display = 'none'
+
+//     }else if(all == 'fruits'){
+//         fruits.style.display = 'block'
+//         cat.style.display = 'none'
+//         dog.style.display = 'none'
+//         horse.style.display = 'none'
+//         nature.style.display = 'none'
+//         vegetables.style.display = 'none'
+//         pop.style.display = 'none'
+//     }else if(all == 'vegetables'){
+//         vegetables.style.display = 'block'
+//         cat.style.display = 'none'
+//         dog.style.display = 'none'
+//         horse.style.display = 'none'
+//         nature.style.display = 'none'
+//         fruits.style.display = 'none'
+//         pop.style.display = 'none'
+//     }else if(all == 'pop'){
+//         pop.style.display = 'block'
+//         cat.style.display = 'none'
+//         dog.style.display = 'none'
+//         horse.style.display = 'none'
+//         nature.style.display = 'none'
+//         fruits.style.display = 'none'
+//         vegetables.style.display = 'none'
+//     }else if(all == 'all'){
+//         cat.style.display = 'block'
+//         dog.style.display = 'block'
+//         horse.style.display = 'block'
+//         nature.style.display = 'block'
+//         fruits.style.display = 'block'
+//         vegetables.style.display = 'block'
+//         pop.style.display = 'block'
+//     }else{
+//         alert('Sorry we do not have this photo')
+//     }
+
+    
+// })
+
+
